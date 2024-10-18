6cd3906ed82fa135d4a9fd7655296eaca642db43
strushkov <strushkov@scrile.com>
2024-10-19 04:24:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab1/ActivityC.kt
@@ -0,0 +1,28 @@
+package com.example.lab1
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ActivityC : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_c)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun onClickNext(view: View) {
+        val intent : Intent = Intent(this, ActivityA::class.java)
+        startActivity(intent)
+    }
+}
